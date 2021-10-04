22928acbb80497a6a0bf51e9a54042c8d232ab5a
tonyponyy <58556009+tonyponyy@users.noreply.github.com>
2021-10-04 12:06:36 +0200

    Eliminados comentarios y formateado el codigo

--- js/ocua.js
+++ js/ocua.js
@@ -1,26 +1,16 @@
-//
-var res;
-
-var  peticion = () => {
-
-	//direccion api ---> https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8
-	fetch('https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8')
-      .then(response => response.json())
-      .then(json => imprimir(json))
-
-}
-
-var imprimir = (num)=>{
-	resultado = num.data[0];
-	porcentaje = (resultado/255)*100
-	sentencia = (resultado> 255/2) ? "si":"no";
-	document.getElementById("texto").innerHTML = "la respuesta es <span id='"+sentencia+"'>"+sentencia+"</span> con un "+porcentaje.toFixed(2)+"%"
-	document.getElementById("boton").innerText = "Otra consulta"
-
-}
-
 var iniciar =() =>{
 	document.getElementById("texto").innerHTML = "Haciendo la consulta..."
 	peticion()
 }
-
+var  peticion = () => {
+	fetch('https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8')
+      .then(response => response.json())
+      .then(json => imprimir(json))
+}
+var imprimir = (num)=>{
+	resultado  = num.data[0]; 
+	porcentaje = (resultado/255)*100
+	sentencia  = (resultado> 255/2) ? "si":"no";
+	document.getElementById("texto").innerHTML = "la respuesta es <span id='"+sentencia+"'>"+sentencia+"</span> con un "+porcentaje.toFixed(2)+"%"
+	document.getElementById("boton").innerText = "Otra consulta"
+}
